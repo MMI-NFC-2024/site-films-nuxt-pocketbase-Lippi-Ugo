6896a1e00c807a876cbc036025d85fd90b956c51
Lippi-Ugo <ugo.lippi@edu.univ-fcomte.fr>
2026-03-02 15:03:57 +0100

    listing des films

--- app/pages/films/index.vue
+++ app/pages/films/index.vue
@@ -1,6 +1,13 @@
 <script setup lang="ts">
+    const NuxtApp = useNuxtApp();
+    const films = await NuxtApp.$pb.collection('Film').getFullList();
 </script>
 
 <template>
     <h1 class="text-4xl">Liste des films</h1>
+    <ul>
+        <li v-for="film in films">
+            {{ film.titre }}
+        </li>
+    </ul>
 </template>
